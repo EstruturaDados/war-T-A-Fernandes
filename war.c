1f9229e6fb6c8e6fdaf0387403db4d0bc7b4bca4
Thiago A. Fernandes <156347671+T-A-Fernandes@users.noreply.github.com>
2025-09-30 20:01:29 +0000

    feat: criar struct Territorio

--- war.c
+++ war.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+// Definição da struct Território
+// Cada território tem um nome, a cor do exército e a quantidade de tropas
+typedef struct {
+    char nome[30];
+    char cor[10];
+    int tropas;
+} Territorio;
+
+
+
 int main() {
     return 0;
 }
