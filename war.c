4b80fc11960540c81887bb375b060d2d107f2044
Thiago A. Fernandes <156347671+T-A-Fernandes@users.noreply.github.com>
2025-09-30 22:48:00 +0000

    feat: implementar entrada de dados dos territórios

--- war.c
+++ war.c
@@ -16,5 +16,26 @@ int main() {
     // Vetor que ira armazenar 5 territórios
     Territorio territorios[5];
 
+    // Entrada de dados
+    printf("Cadastro de 5 territórios:\n");
+    for(int i = 0; i < 5; i++) {
+        printf("\nTerritório %d:\n", i + 1);
+
+        // Lendo o nome do território
+        printf("Digite o nome do território: ");
+        fgets(territorios[i].nome, sizeof(territorios[i].nome), stdin); // fgets permite ler strings com espaços
+        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = 0; // Remove o '\n' final da string
+
+        // Lendo a cor do exército
+        printf("Digite a cor do exército: ");
+        fgets(territorios[i].cor, sizeof(territorios[i].cor), stdin);
+        territorios[i].cor[strcspn(territorios[i].cor, "\n")] = 0; 
+
+        // Lendo número de tropas
+        printf("Digite o número de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+        getchar(); // Limpa o '\n' deixado pelo scanf
+    }
+
     return 0;
 }
