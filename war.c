8be1fdb0f16681d0b2f3efe73fb8fe2b6e0b36ba
Thiago A. Fernandes <156347671+T-A-Fernandes@users.noreply.github.com>
2025-10-10 18:37:27 +0000

    feat: função principal

--- war.c
+++ war.c
@@ -91,47 +91,57 @@ void liberarMemoria(Territorio* mapa){
     free(mapa);
 }
 
-
-
+// ==============================
+// Função: Principal
+// ==============================
 int main() {
+    srand(time(NULL)); // Aleatóriedade
 
-    // Vetor que ira armazenar 5 territórios
-    Territorio territorios[5];
+    int qtd;
+    printf("Quantos territórios deseja cadastrar? ");
+    scanf("%d", &qtd);
+    getchar();
 
-    // Entrada de dados
-    printf("==============================================================\n");
-    printf("Vamos cadastrar os 5 territórios iniciais do nosso mundo.\n");
-    printf("==============================================================\n");
-    for(int i = 0; i < 5; i++) {
-        printf("\n--- Cadastranto Território %d ---\n", i + 1);
-
-        // Lendo o nome do território
-        printf("Nome do território: ");
-        fgets(territorios[i].nome, sizeof(territorios[i].nome), stdin); // fgets permite ler strings com espaços
-        territorios[i].nome[strcspn(territorios[i].nome, "\n")] = 0; // Remove o '\n' final da string
-
-        // Lendo a cor do exército
-        printf("Cor do exército: ");
-        fgets(territorios[i].cor, sizeof(territorios[i].cor), stdin);
-        territorios[i].cor[strcspn(territorios[i].cor, "\n")] = 0; 
-
-        // Lendo número de tropas
-        printf("Número de tropas: ");
-        scanf("%d", &territorios[i].tropas);
-        getchar(); // Limpa o '\n' deixado pelo scanf
+    // Alocação do vetor de territórios
+    Territorio* mapa = (Territorio*) calloc(qtd, sizeof(Territorio));
+    if (mapa == NULL) {
+        printf("Erro ao alocar mamória!\n");
+        return 1;
     }
+    
+    // Cadastro
+    cadastrarTerritorios(mapa, qtd);
 
-    // Exibição dos dados
-    printf("\n==================================\n");
-    printf("Mapa do Mundo - ESTADO ATUAL\n");
-    printf("==================================\n");
+    int opcao;
+    do {
+        exibirMapa(mapa, qtd);
+        printf("\nO que deseja fazer?\n");
+        printf("1 - Atacar\n");
+        printf("2 - sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+        getchar();
 
-    for(int i = 0; i < 5; i++) {
-        printf("\nTerritório %d\n", i + 1); 
-        printf("   - Nome: %s\n", territorios[i].nome);
-        printf("   - Dominado por: %s\n", territorios[i].cor);
-        printf("   - Tropas: %d\n", territorios[i].tropas);
-    }
+        if (opcao == 1) {
+            int iAtacante, iDefensor;
 
+            printf("\nEscolha o número do território atacante: ");
+            scanf("%d", &iAtacante);
+            getchar();
+
+            printf("Escolha o número do território defensor: ");
+            scanf("%d", &iDefensor);
+            getchar();
+
+            if (iAtacante < 1 || iAtacante > qtd || iDefensor < 1 || iDefensor > qtd) {
+                printf("\n⚠️ Índices invalidos!\n");
+            } else {
+                atacar(&mapa[iAtacante - 1], &mapa[iDefensor - 1]);
+            }
+        }
+    } while (opcao != 2);
+    liberarMemoria(mapa);
+    printf("\nMemória liberada. Fim de Jogo!\n");
+    
     return 0;
 }
